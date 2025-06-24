be1496aa5a4c0a838fd31241ed12a23b8f2bdafe
ISAAC2224 <105130214+ISAAC2224@users.noreply.github.com>
2025-06-24 10:31:58 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,61 +1,61 @@
+// server.js
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const stripe = require("stripe")("sk_test_51RdP4J2MvrNawmXjU24bHXkhgHQJQrU4Y1Pq2sszf7gGravcbZJxN2jVe7XgTkuED9lPuBHA3f1eZbTuAzJ05C3x00BAxDmCcw");
+const stripe = require("stripe")("sk_test_...REPLACE_WITH_YOURS...");
 
 app.use(cors());
 app.use(express.json());
+app.use('/images', express.static('images'));
 
 const productData = {
+  "Jordan 5 Metallic": { price: 27500, image: "jordan5_front.png" },
+  "AF1 CPFM Fuchsia Dream": { price: 57500, image: "cpfm_fuchsia_1.png" },
+  "AF1 CPFM White": { price: 55000, image: "cpfm_white_1.png" },
+  "Multicolor Balenciaga runners": { price: 80000, image: "balenciaga_runner.png" },
+  "Rick Owens Porterville": { price: 65000, image: "rickowens_1.png" },
+  "Zoom Kobe 6 Protro Dodgers": { price: 28500, image: "ZK61.png" },
+  "Bottega Veneta Orbit": { price: 95000, image: "BVO1.png" },
+  "Lil Yachty x Air Force 1 Concrete Boys Lucky Green": { price: 23000, image: "LYXAF1.png" },
+  "Union LA x Air Jordan 4 Retro Off Noir": { price: 38500, image: "UNION1.png" },
+  "Travis Scott x AJ1 Low OG Black Phantom": { price: 58500, image: "TSAJ1.png" },
+  "Jordan 14 Retro Ferrari": { price: 24500, image: "J141.png" },
+  "Rick Owen Vintage Low Brown": { price: 59000, image: "ROVLB1.png" },
+  "Supreme x Dunk Low SB Rammellzee": { price: 27500, image: "SXDL1.png" },
+  "Rick Owens DRKSHDW Luxor Low Black Pearl": { price: 65000, image: "DRKSHDW1.png" },
+  "Off White Slides black and yellow": { price: 18500, image: "OFFW1.png" },
+  "Rick Owens Vintage suede-trimmed leather sneakers": { price: 60000, image: "leathersneakers1.png" },
+  "Balenciaga Black Furry Slides": { price: 55000, image: "BBFS1.png" },
+  "Ben & Jerrys x Dunk Low SB Chunky Dunky": { price: 95000, image: "B&J1.png" },
+  "Marni Slippers (Multiple Colors)": { price: 50000, image: "pinkmarni.png" },
   "Chrome Hearts Spider Web Long Sleeve": { price: 45000, image: "CHSWLS1.png" },
   "Chrome Hearts Made In Hollywood LS": { price: 40000, image: "CHMBSWLS1.png" },
   "BAPE Color Camo Shark Zip Hoodie (Purple)": { price: 26000, image: "Purple1.png" },
   "BAPE Color Camo Shark Zip Hoodie (Red)": { price: 30000, image: "Red1.png" },
   "BAPE ABC Camo Shark Zip Hoodie (Blue)": { price: 27500, image: "Blue1.png" },
   "BAPE ABC Camo Shark Zip Hoodie (Pink)": { price: 27500, image: "Pink1.png" },
-  "BAPE Multi Camo NYC Shark Zip Hoodie (Black)": { price: 60000, image: "BMCNYC1.png" },
-  "Marni Slippers (Multiple Colors)": { price: 50000, image: "pinkmarni.png" },
-  "Ben & Jerrys x Dunk Low SB Chunky Dunky": { price: 95000, image: "B&J1.png" },
-  "Balenciaga Black Furry Slides": { price: 55000, image: "BBFS1.png" },
-  "Rick Owens Vintage suede-trimmed leather sneakers": { price: 60000, image: "leathersneakers1.png" },
-  "Off White Slides black and yellow": { price: 18500, image: "OFFW1.png" },
-  "Rick Owens DRKSHDW Luxor Low Black Pearl": { price: 65000, image: "DRKSHDW1.png" },
-  "Supreme x Dunk Low SB Rammellzee": { price: 27500, image: "SXDL1.png" },
-  "Rick Owen Vintage Low Brown": { price: 59000, image: "ROVLB1.png" },
-  "Jordan 14 Retro Ferrari": { price: 24500, image: "J141.png" },
-  "Travis Scott x AJ1 Low OG Black Phantom": { price: 58500, image: "TSAJ1.png" },
-  "Powerpuff Girls Blossom Nike SB Dunks": { price: 30000, image: "PGB1.png" },
-  "Union LA x Air Jordan 4 Retro Off Noir": { price: 38500, image: "UNION1.png" },
-  "Lil Yachty x Air Force 1 Concrete Boys Lucky Green": { price: 23000, image: "LYXAF1.png" },
-  "Bottega Veneta Orbit": { price: 95000, image: "BVO1.png" },
-  "Zoom Kobe 6 Protro Dodgers": { price: 28500, image: "ZK61.png" },
-  "Rick Owens Porterville": { price: 65000, image: "rickowens_1.png" },
-  "AF1 CPFM White": { price: 55000, image: "cpfm_white_1.png" },
-  "Multicolor Balenciaga runners": { price: 80000, image: "balenciaga_runner.png" },
-  "Jordan 5 Metallic": { price: 27500, image: "jordan5_front.png" },
-  "AF1 CPFM Fuchsia Dream": { price: 57500, image: "cpfm_fuchsia_1.png" }
+  "BAPE Multi Camo NYC Shark Zip Hoodie (Black)": { price: 60000, image: "BMCNYC1.png" }
 };
 
 app.post("/create-checkout-session", async (req, res) => {
-  const items = req.body.items;
-  const lineItems = [];
+  const { items } = req.body;
 
-  for (let item of items) {
+  const lineItems = items.map((item) => {
     const product = productData[item.name];
-    if (!product) {
-      return res.status(400).json({ error: "Product not found." });
-    }
-    lineItems.push({
+    return {
       price_data: {
         currency: "usd",
         product_data: {
           name: item.name,
+          images: [
+            `https://surfzresell.com/images/${product.image}`
+          ]
         },
         unit_amount: product.price,
       },
       quantity: 1,
-    });
-  }
+    };
+  });
 
   try {
     const session = await stripe.checkout.sessions.create({
@@ -63,10 +63,10 @@ app.post("/create-checkout-session", async (req, res) => {
       line_items: lineItems,
       mode: "payment",
       success_url: "https://www.surfzresell.com/success",
-cancel_url: "https://www.surfzresell.com/cancel",
-
+      cancel_url: "https://www.surfzresell.com/cancel",
     });
-    res.json({ id: session.id });
+
+    res.json({ url: session.url });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
